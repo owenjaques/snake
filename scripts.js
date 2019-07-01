c383eb86fe9886a538e40500956172777d40e8a4
owenjaques <owenjaques2000@gmail.com>
2019-07-01 13:05:42 -0700

    added the ability to restart increased speed

--- scripts.js
+++ scripts.js
@@ -1,13 +1,6 @@
-var canvas = document.getElementById("gameArea");
-var ctx = canvas.getContext("2d");
-var dx = squareSize = 10;
-var dy = score = 0;
-var screenWidth = 600;
-var screenHeight = 400;
+var canvas, ctx, dx, dy, squareSize, score, screenWidth, screenHeight, snake, food, interval, started, gameOver;
 
-var snake = [ {x:250, y:250}, {x:250, y:260}, {x:250, y:270}, {x:250, y:280}];
-var food = {x: randCoor(screenWidth), y: randCoor(screenHeight)};
-
+//draws on canvas
 function draw(){
 	//draws background
 	ctx.fillStyle = "aqua";
@@ -16,7 +9,6 @@ function draw(){
 	ctx.strokeRect(0, 0, screenWidth, screenHeight);
 	//draws food
 	ctx.fillStyle = "red";
-	ctx.strokestyle = "darkred";
 	ctx.fillRect(food.x, food.y, squareSize, squareSize);
 	ctx.strokeRect(food.x, food.y, squareSize, squareSize);
 	//draws snake
@@ -28,9 +20,12 @@ function draw(){
 	}
 }
 
+//effectively moves the snake by adding the next location to a list array and popping the last element
 function moveSnake(){
 	var head = {x:snake[0].x + dx, y:snake[0].y + dy};
 	snake.unshift(head);
+
+	//if it hits food do not pop
 	if(snake[0].x == food.x && snake[0].y == food.y){
 		createFood();
 		score++;
@@ -66,6 +61,7 @@ function getDirection(){
 
 document.addEventListener("keydown", getDirection);
 
+//generates a random number within the screen size given that is a multiple of ten
 function randCoor(size){
 	return Math.round((Math.random() * (size - squareSize)) / 10) * 10;
 }
@@ -84,6 +80,7 @@ function createFood(){
 	food.y = y;
 }
 
+//writes to the html
 function write(){
 	var ele = document.getElementById("snakex");
 	ele.innerHTML = snake[0].x;
@@ -101,22 +98,71 @@ function checkCol(){
 	//checks for collision with wall
 	if(snake[0].x == -10 || snake[0].x == screenWidth || snake[0].y == -10 || snake[0].y == screenHeight){
 		clearInterval(interval);
-		document.getElementById("gameOver").innerHTML = "Snake!!! - Game Over";
+		gameOver = true;
+		return;
 	}
 	//checks for collision with self
 	for(i = 1; i < snake.length; i++){
 		if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
 			clearInterval(interval);
-			document.getElementById("gameOver").innerHTML = "Snake!!! - Game Over";
+			gameOver = true;
+			return;
 		}
 	}
 }
 
+//a helper function to display a message to the canvas
+function displayMessage(message){
+	ctx.font = "bold 30px Arial";
+	ctx.fillStyle = "white";
+	ctx.strokeStyle = "grey";
+	ctx.fillText(message, 25, screenHeight/2);
+	ctx.strokeText(message, 25, screenHeight/2);
+}
+
+function init(){
+	canvas = document.getElementById("gameArea");
+	ctx = canvas.getContext("2d");
+	dx = squareSize = 10;
+	dy = score = 0;
+	screenWidth = 600;
+	screenHeight = 400;
+	snake = [ {x:250, y:250}, {x:250, y:260}, {x:250, y:270}, {x:250, y:280}];
+	food = {x: randCoor(screenWidth), y: randCoor(screenHeight)};
+	if(gameOver){
+		displayMessage("Game Over - Press any key to Restart");
+		//waits for one second to avoid immidiatly restarting if keys are accidentaly pressed
+		setTimeout(function() {started = false;}, 1000);
+	}
+	else {
+		draw();
+		displayMessage("Press any key to Start");
+		started = false;
+	}
+	gameOver = false;
+}
+
+function start(){
+	//so that the interval is not set multiple times making it faster and faster
+	if(!started){
+		interval = setInterval(main, 100);
+		started = true;
+	}
+}
+
+//the function that is looped
 function main(){
 	moveSnake();
 	write();
 	checkCol();
 	draw();
+	if(gameOver)
+		init();
 }
 
-var interval = setInterval(main, 125);
+init();
+
+//if a key is pressed the game starts or restarts
+document.body.onkeyup =  start;
+
+
